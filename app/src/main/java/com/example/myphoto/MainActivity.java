30d5e90f973bb95792b0eeeedc927e5a71f92465
Yunfei.Yan.Hp <environment2013@163.com>
2019-10-26 11:28:05 +0800

    Prompt for user to specify a name for the image after taking a photo.

--- app/src/main/java/com/example/myphoto/MainActivity.java
+++ app/src/main/java/com/example/myphoto/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myphoto;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -8,9 +9,11 @@ import android.os.Build;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.myphoto.persistence.PhotoRecord;
 import com.example.myphoto.persistence.SPPhotoAlbum;
@@ -54,9 +57,34 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE_TAKE_PHOTO) {
             if (resultCode == RESULT_OK) {
-                syncPhotoInfo();
+                showInputDialog();
             }
         }
+    }
+
+    private void showInputDialog() {
+        final EditText inputServer = new EditText(this);
+        inputServer.setFocusable(true);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.custom_image_name)).setView(inputServer);
+        builder.setNegativeButton(
+                getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        customImageName = "";
+                        syncPhotoInfo();
+                    }
+                });
+        builder.setPositiveButton(getString(R.string.ok),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        String inputName = inputServer.getText().toString();
+                        customImageName = inputName;
+                        syncPhotoInfo();
+                    }
+                });
+        builder.show();
     }
 
     private void syncPhotoInfo() {
